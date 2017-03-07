9ccc08ab47067765a93765112d9a267383f22813
SecondThread <13dharmeyer@gmail.com>
2017-03-06 21:31:25 -0600

    Added sky background

--- src/scenes/MainScene.java
+++ src/scenes/MainScene.java
@@ -3,6 +3,8 @@ package scenes;
 import java.util.ArrayList;
 import java.util.List;
 
+import graphics.Camera;
+import graphics.Sprite;
 import javafx.scene.canvas.GraphicsContext;
 import math.Vector2;
 import objects.GameObject;
@@ -12,11 +14,13 @@ import objects.tiles.GrassTile;
 public class MainScene extends Scene {
 	
 	private static List<GameObject> gameObjects;
+	private static Sprite sky;
 	
 	public MainScene() {
 		gameObjects=new ArrayList<>();
 		gameObjects.add(new GrassTile(new Vector2(100, 350), 400, 50));
 		gameObjects.add(new Player(new Vector2(400, 0)));
+		sky=Sprite.getSprite("sky.png");
 	}
 	
 	public Scene update() {
@@ -27,6 +31,7 @@ public class MainScene extends Scene {
 	}
 
 	public void render(GraphicsContext gc) {
+		sky.draw(gc, Camera.cameraPosition);
 		for (int i=0; i<gameObjects.size(); i++) {
 			gameObjects.get(i).render(gc);;
 		}
