65272f1bb0ee50da3824f1d8bf62d179ee59a536
SecondThread <13dharmeyer@gmail.com>
2017-03-06 21:23:19 -0600

    working on player movement

--- src/objects/Player.java
+++ src/objects/Player.java
@@ -3,6 +3,7 @@ package objects;
 import java.util.List;
 
 import graphics.Sprite;
+import input.Keyboard;
 import javafx.geometry.BoundingBox;
 import javafx.scene.canvas.GraphicsContext;
 import math.Vector2;
@@ -13,8 +14,9 @@ public class Player extends GameObject implements Collidable {
 	private Vector2 position, velocity;
 	private double width, height;
 	private Sprite playerSprite;
-	private static final Vector2 gravity=new Vector2(0, 0.2);
-	private static final double friction=0.98;
+	private static final Vector2 gravity=new Vector2(0, 0.4);
+	private static final double friction=0.8, speed=1, jumpPower=10;
+	private boolean grounded=false;
 	
 	public Player(Vector2 position) {
 		this.position=position;
@@ -25,22 +27,19 @@ public class Player extends GameObject implements Collidable {
 	}
 	
 	public void update() {
+		if (Keyboard.getKeyDown('D')) {
+			velocity=new Vector2(velocity.getX()+speed, velocity.getY());
+		}
+		if (Keyboard.getKeyDown('A')) {
+			velocity=new Vector2(velocity.getX()-speed, velocity.getY());
+		}
+		if (Keyboard.getKeyPressed('W')) {
+			velocity=new Vector2(velocity.getX(), -jumpPower);
+		}
+		
 		velocity=velocity.add(gravity);
-		velocity=velocity.scaleBy(friction);
-		if (moveX(MainScene.getGameObjects())) {
-			//move to touch
-			velocity=new Vector2(0.01*Math.signum(velocity.getX()), velocity.getY());
-			while (!moveX(MainScene.getGameObjects())&&velocity.getX()!=0) {
-			}
-			velocity=new Vector2(0, velocity.getY());
-		}
-		if (moveY(MainScene.getGameObjects())) {
-			//move to touch
-			velocity=new Vector2(velocity.getX(), 0.01*Math.signum(velocity.getY()));
-			while (!moveY(MainScene.getGameObjects())&&velocity.getY()!=0) {
-			}
-			velocity=new Vector2(Math.signum(velocity.getX()), 0);
-		}
+		velocity=new Vector2(velocity.getX()*friction, velocity.getY());
+		move();
 	}
 	
 	public void render(GraphicsContext gc) {
@@ -56,9 +55,27 @@ public class Player extends GameObject implements Collidable {
 		return toReturn;
 	}
 	
+	private void move() {
+		if (moveX(MainScene.getGameObjects())) {
+			//move to touch
+			velocity=new Vector2(0.01*Math.signum(velocity.getX()), velocity.getY());
+			while (!moveX(MainScene.getGameObjects())&&velocity.getX()!=0) {
+			}
+			velocity=new Vector2(0, velocity.getY());
+		}
+		if (moveY(MainScene.getGameObjects())) {
+			//move to touch
+			velocity=new Vector2(velocity.getX(), 0.01*Math.signum(velocity.getY()));
+			while (!moveY(MainScene.getGameObjects())&&velocity.getY()!=0) {
+			}
+			velocity=new Vector2(velocity.getX(), 0);
+			grounded=true;
+		}
+	}
+	
 	/**
 	 * Returns whether or not there was a collision
-	 * @param objects
+	 * @param objects a list of objects that it is possible to collide with
 	 * @return true if and only if there was a collision
 	 */
 	private boolean moveX(List<GameObject> objects) {
@@ -77,6 +94,11 @@ public class Player extends GameObject implements Collidable {
 		return false;
 	}
 	
+	/**
+	 * Returns whether or not there was a collision
+	 * @param objects a list of objects that it is possible to collide with
+	 * @return true if and only if there was a collision
+	 */
 	private boolean moveY(List<GameObject> objects) {
 		Vector2 oldPosition=position;
 		double newYPosition=position.getY()+velocity.getY();
